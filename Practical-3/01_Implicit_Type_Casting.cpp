f3b5d23cbbf379e2190ab581c81e3801527613a9
rohitsureshmadake-pixel <rohitsureshmadake@gmail.com>
2026-08-20 23:41:18 +0530

    Add Practical 3 implicit type casting program

---
+++ Practical-3/01_Implicit_Type_Casting.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    double b = 5.5;
+    double result = a + b;
+
+    cout << "Integer value: " << a << endl;
+    cout << "Double value: " << b << endl;
+    cout << "Result after implicit casting: " << result << endl;
+
+    return 0;
+}
