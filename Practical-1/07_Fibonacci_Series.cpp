53da9aa92d39180b9c19b90bf6be10b7d6de8206
rohitsureshmadake-pixel <rohitsureshmadake@gmail.com>
2026-08-20 22:08:33 +0530

    Add C++ solution for Fibonacci series

---
+++ Practical-1/07_Fibonacci_Series.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    long long first = 0, second = 1, next;
+
+    cout << "Enter number of terms: ";
+    cin >> n;
+
+    cout << "Fibonacci Series: ";
+    for (int i = 1; i <= n; i++) {
+        cout << first << " ";
+        next = first + second;
+        first = second;
+        second = next;
+    }
+
+    cout << endl;
+    return 0;
+}
