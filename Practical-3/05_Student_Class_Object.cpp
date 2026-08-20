6946c284697c1258f1e10a617aaa3345898d1190
rohitsureshmadake-pixel <rohitsureshmadake@gmail.com>
2026-08-20 23:41:45 +0530

    Add Practical 3 Student class and object program

---
+++ Practical-3/05_Student_Class_Object.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student {
+    int rollNo;
+    string name;
+    float marks;
+
+public:
+    void input() {
+        cout << "Enter roll number: ";
+        cin >> rollNo;
+        cout << "Enter name: ";
+        cin >> name;
+        cout << "Enter marks: ";
+        cin >> marks;
+    }
+
+    void display() {
+        cout << "\nStudent Information\n";
+        cout << "Roll No: " << rollNo << endl;
+        cout << "Name: " << name << endl;
+        cout << "Marks: " << marks << endl;
+    }
+};
+
+int main() {
+    Student s;
+    s.input();
+    s.display();
+    return 0;
+}
